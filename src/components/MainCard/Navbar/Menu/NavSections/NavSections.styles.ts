c0b5a0d296975ea5ce77e70aeaf5c1d75c9b4355
Morghot2 <maximilian.jaron@gmail.com>
2023-10-01 01:57:34 +0200

    dc-000010: added button effects for desktop navbar

--- src/components/MainCard/Navbar/Menu/NavSections/NavSections.styles.ts
+++ src/components/MainCard/Navbar/Menu/NavSections/NavSections.styles.ts
@@ -7,13 +7,21 @@ const NavSectionsWrapper = styled.section`
   gap: 60px;
 `;
 
-const NavItem = styled.div`
+const NavItem = styled.button`
   font-family: 'Poppins', sans-serif;
   font-family: Poppins;
   font-size: 22px;
   font-weight: 500;
+  color: #224f34;
   line-height: 33px;
   cursor: pointer;
+  background-color: transparent;
+  transition: 0.5s;
+  border: none;
+  padding: 10px;
+  &:hover {
+    font-size: 26px;
+  }
 `;
 
 export { NavSectionsWrapper, NavItem };
